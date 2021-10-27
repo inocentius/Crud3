fc139ee1813ee36264774d1208b995838aca2781
Inocentius.works <inocentius.works@gmail.com>
2021-10-27 10:35:42 +0700

    new update

--- routes/index.js
+++ routes/index.js
@@ -1,106 +1,108 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ObjectId = require('mongoose').Types.ObjectId;    
 
-const { Employee } = require('../models/employee');
+const { Employee } = require("../models/employee");
 
-
-// Get All Employees
-router.get('/api/employees', (req, res) => {
-    Employee.find({}, (err, data) => {
-        if(!err) {
-            res.send(data);
-        } else {
-            console.log(err);
-        }
-    });
+//get All Employees
+router.get("/api/employee", (req, res) => {
+  Employee.find({}, (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      res.status(404).json({
+        code : 404,
+        message : "Error",
+      })
+    }
+  });
 });
 
-
-// Get Single Employee (First Way)
-
-router.get('/api/employees/:id', (req, res) => {
-    Employee.findById(req.params.id, (err, data) => {
-        if(!err) {
-            res.send(data);
-        } else {
-           console.log(err);
-        }
-    });
+//get single Employee
+router.get("/api/employee/:id", (req, res) => {
+  Employee.findById(req.params.id, (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      res.status(404).json({
+        code : 404,
+        message : "Error",
+      });
+    }
+  });
+});
+//save Employee
+router.post("/api/employee/add", (req, res) => {
+  const emp = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    salary: req.body.salary,
+  });
+  emp.save((err, data) => {
+    if(!err) {
+      res
+      .status(200)
+      .json({
+        code: 200,
+        message: "Employee Added Successfully",
+        addEmployee: data,
+      });
+    } else {
+      res.status(404).json({
+        code : 404,
+        message : "Error",
+      })
+    }
+  });
 });
 
-
-// Get Single Employee (2nd Way)
-
-// router.get('/api/employee/:id', (req, res) => {
-//     if(!ObjectId.isValid(req.params.id))
-//     return res.status(400).send(`No record With Given ID : ${req.params.id}`);
-
-//     Employee.findById(req.params.id, (err, data) => {
-//         if(!err) {
-//             res.send(data);
-//         } else {
-//            console.log(err);
-//         }
-//     });
-// });
-
-
-// Save Employee
-router.post('/api/employee/add', (req, res) => {
-    const emp = new Employee({
-        name: req.body.name,
-        email: req.body.email,
-        salary: req.body.salary
-    });
-    emp.save((err, data) => {
-        if(!err) {
-            // res.send(data);
-            res.status(200).json({code: 200, message: 'Employee Added Successfully', addEmployee: data})
-        } else {
-           console.log(err);
-        }
-    });
+//update Employee
+router.put("/api/employee/edit/:id", (req, res) => {
+  const emp = {
+    name: req.body.name,
+    email: req.body.email,
+    salary: req.body.salary,
+  };
+  Employee.findByIdAndUpdate(
+    req.params.id,
+    { $set: emp },
+    { new: true },
+    (err, data) => {
+      if (!err) {
+        res
+          .status(200)
+          .json({
+            code: 200,
+            message: "Employee Updated Successfully",
+            updateEmployee: data,
+          });
+      } else {
+        res.status(404).json({
+          code : 404,
+          message : "Error",
+        })
+      }
+    }
+  );
 });
 
-
-
-// Update Employee
-
-router.put('/api/employee/update/:id', (req, res) => {
-
-
-    const emp = {
-        name: req.body.name,
-        position: req.body.position,
-        office: req.body.office,
-        salary: req.body.salary
-    };
-    Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true }, (err, data) => {
-        if(!err) {
-            res.status(200).json({code: 200, message: 'Employee Updated Successfully', updateEmployee: data})
-        } else {
-            console.log(err);
-        }
-    });
+//Delete Employee
+router.delete("/api/employee/:id", (req, res) => {
+  Employee.findByIdAndRemove(req.params.id, (err, data) => {
+    if (!err) {
+      res
+        .status(200)
+        .json({
+          code: 200,
+          message: "Employee Delete Successfully",
+          deleteEmployee: data,
+        });
+    } else {
+      res.status(404).json({
+        code : 404,
+        message : "Error",
+      })
+    }
+  });
 });
-
-
-
-
-
-// Delete Employee
-router.delete('/api/employee/:id', (req, res) => {
-
-    Employee.findByIdAndRemove(req.params.id, (err, data) => {
-        if(!err) {
-            // res.send(data);
-            res.status(200).json({code: 200, message: 'Employee deleted', deleteEmployee: data})
-        } else {
-            console.log(err);
-        }
-    });
-});
-
 
 module.exports = router;
